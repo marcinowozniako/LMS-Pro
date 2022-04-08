2713d0b4b1b2b0be3e56424f447982ac78bf01ca
marcinowozniako <marekboniek@gmail.com>
2022-04-08 15:08:01 +0200

    add courses form and support user permissions

--- home/views.py
+++ home/views.py
@@ -4,6 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import TemplateView
 
+from courses.models import Course, Subject
+
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['courses'] = Course.objects.all()
+        ctx['subject'] = Subject.objects.all()
+
+        return ctx
