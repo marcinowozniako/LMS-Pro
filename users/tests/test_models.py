73174b92c0decf6c8f17817e623b19b542f86216
marcinowozniako <marekboniek@gmail.com>
2022-04-08 11:35:14 +0200

    add custom migrations for groups and superuser from envs

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -1,7 +1,6 @@
 def test_create_user(user, django_user_model):
     users = django_user_model.objects.all()
-    assert len(users) == 1
-    assert users[0].fullname == 'Ala'
+    assert len(users) == 2
 
 
 def test_change_password(user):
