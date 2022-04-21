50b688088e780e65c974b0f158f945c6a1228212
marcinowozniako <marekboniek@gmail.com>
2022-04-21 23:10:17 +0200

    final

--- conftest.py
+++ conftest.py
@@ -5,4 +5,4 @@ from django.contrib.auth import get_user_model
 @pytest.fixture(scope='function')
 def user(db, django_user_model):
     """User instance from default django user model"""
-    yield django_user_model.objects.create_user(email='a1@a.pl', fullname='Ala', password='testPass123')
+    yield django_user_model.objects.create_user(email='a11@a.pl', fullname='Ala', password='testPass123')
